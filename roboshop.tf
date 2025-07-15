3ff309aa15906556828a1410b45350cb7d14d6cb
Mainul Islam <mainulislam6168@gmail.com>
2025-07-15 23:32:01 +0600

    used loop to create instances in roboshop

--- roboshop.tf
+++ roboshop.tf
@@ -5,102 +5,18 @@ variable "ami" {
 variable "instance_type" {
   default = "t3.small"
 }
+variable "instances" {
+  default = ["frontend","catalogue","cart", " user", "shipping", "payment", "mongodb","redis", "rabbitmq","mysql" ]
+}
+
 # 1
-resource "aws_instance" "frontend" {
+resource "aws_instance" "instance" {
+  count = length(var.instances)
   ami           = var.ami
   instance_type = var.instance_type
 
   tags = {
-    Name = "frontend"
-  }
-}
-# 2
-resource "aws_instance" "mongodb" {
-  ami           = var.ami
-  instance_type = var.instance_type
-
-  tags = {
-    Name = "mongodb"
-  }
-}
-# 3
-resource "aws_instance" "cart" {
-  ami           = var.ami
-  instance_type = var.instance_type
-
-  tags = {
-    Name = "cart"
-  }
-}
-# 4
-resource "aws_instance" "catalogue" {
-  ami           = var.ami
-  instance_type = var.instance_type
-
-  tags = {
-    Name = "catalogue"
-  }
-}
-# 5
-resource "aws_instance" "dispatch" {
-  ami           = var.ami
-  instance_type = var.instance_type
-
-  tags = {
-    Name = "dispatch"
-  }
-}
-# 6
-resource "aws_instance" "mysql" {
-  ami           = var.ami
-  instance_type = var.instance_type
-
-  tags = {
-    Name = "mysql"
-  }
-}
-# 7
-resource "aws_instance" "payment" {
-  ami           = var.ami
-  instance_type = var.instance_type
-
-  tags = {
-    Name = "payment"
-  }
-  # 8
-}resource "aws_instance" "rabbitmq" {
-  ami           = var.ami
-  instance_type = var.instance_type
-
-  tags = {
-    Name = "rabbitmq"
-  }
-}
-# 9
-resource "aws_instance" "redis" {
-  ami           = var.ami
-  instance_type = var.instance_type
-
-  tags = {
-    Name = "redis"
-  }
-}
-# 10
-resource "aws_instance" "shipping" {
-  ami           = var.ami
-  instance_type = var.instance_type
-
-  tags = {
-    Name = "shipping"
-  }
-}
-# 11
-resource "aws_instance" "user" {
-  ami           = var.ami
-  instance_type = var.instance_type
-
-  tags = {
-    Name = "user"
+    Name = var.instances[count.index]
   }
 }
 
